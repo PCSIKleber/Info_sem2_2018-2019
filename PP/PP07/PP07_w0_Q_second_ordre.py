b294b9d9da9e083283e8eee3abbb5328384d2a52
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-01-12 09:53:52 +0100

    Arrivée du PP07

---
+++ PP/PP07/PP07_w0_Q_second_ordre.py
@@ -0,0 +1,16 @@
+# Pour la lecture
+import numpy as np
+# Lecture des donnees
+donnees_t,donnees_u = np.loadtxt('Uc_41.txt',unpack=True)
+
+# Les trois derniers exercices vous permettent de determiner tout ce qu'il 
+# faut pour calculer w0 et Q. En particulier, la pseudo-periode est reliee a 
+# w0 et Q, ainsi que le temps caracteristique tau de decroissance: cherchez 
+# dans vos cours de physique les relations adequates et inversez-les pour 
+# trouver w0 et Q en fonction de T et tau.
+
+w0 = 'Une certaine valeur'
+Q  = 'Une autre'
+
+# Affichage des resultats demandes
+print(w0,Q)
