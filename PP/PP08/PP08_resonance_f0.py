b736efa67b4de8e6c7a9902fda35cd2306da2710
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-01-17 21:49:10 +0100

    Arrivée du PP08

---
+++ PP/PP08/PP08_resonance_f0.py
@@ -0,0 +1,15 @@
+# Importation de Numpy pour la lecture
+import numpy as np
+# Lecture des donnees
+f,i,u = np.loadtxt('tp_resonance_ampoule.txt',unpack=True)
+
+# En regardant l'evolution de l'intensite i en fonction de la frequence f, 
+# determiner la valeur de la frequence propre f0 du systeme (il s'agit d'une 
+# resonance en intensite) par la methode de votre choix.
+
+f0 = 'a trouver'
+
+
+
+# On affiche le resultat
+print(f0)
