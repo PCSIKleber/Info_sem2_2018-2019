fcd8e07fb7ba6c2329c76ae66a87ba8765c2da9b
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-01-25 18:25:16 +0100

    Arrivée du PP09

---
+++ PP/PP09/PP09_resonance_Q.py
@@ -0,0 +1,14 @@
+# Importation de Numpy pour la lecture
+import numpy as np
+# Lecture des donnees
+f,i = np.loadtxt('tp_resonance_ampoule.txt',unpack=True,usecols=(0,1))
+
+# En regardant l'evolution de l'intensite i en fonction de la frequence f, 
+# determiner la valeur du facteur de qualite (il s'agit d'une resonance en 
+# intensite) par la methode de votre choix.
+
+Q = 'a trouver'
+
+
+# On affiche le resultat
+print(Q)
