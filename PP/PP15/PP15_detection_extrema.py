1fdc6c1202e525055c8d528a70029538e2c58672
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-03-22 07:55:43 +0100

    Arrivée du PP15, détection de positions d'équilibre

---
+++ PP/PP15/PP15_detection_extrema.py
@@ -0,0 +1,12 @@
+# Importation de la fonction adequate
+from Ep import Ep
+
+
+# A vous de trouver les extrema (on ne sait pas combien il y en a)
+
+
+extrema = ['non','pas','ca']
+
+
+# Affichage des resultats
+print(*extrema)
