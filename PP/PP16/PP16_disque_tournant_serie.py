8f77cc05689600a18d925ceedfd3de4399bd2823
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-03-28 08:39:15 +0100

    Arrivée du PP16, travail en série sur des fichiers

---
+++ PP/PP16/PP16_disque_tournant_serie.py
@@ -0,0 +1,11 @@
+# Import de Numpy qui sert a lire le fichier
+import numpy as np
+
+# A vous de jouer en rassemblant dans une fonction applicable a tous les 
+# fichiers renvoyes par glob le resultat de la regression linaire adequate
+# Pensez a vous inspirer de ce qui a ete fait dans le PP sur le disque tournant.
+
+liste_des_coeffs = ['ce','qui','doit','etre','rempli']
+
+# A la fin, on affiche les resultats a l'ecran
+print(*liste_des_coeffs)
