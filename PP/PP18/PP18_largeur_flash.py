b77d1db887a01d0dbd543438a08b1aa0af5e8498
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-04-24 18:12:00 +0200

    Arrivée du PP18, largeur d'un flash

---
+++ PP/PP18/PP18_largeur_flash.py
@@ -0,0 +1,15 @@
+# Import pour lecture
+import numpy as np
+# Lecture effective des donnees
+t,u = np.loadtxt('flash.txt',unpack=True,skiprows=1)
+
+# A vous de determiner la largeur du flash (definie comme l'interalle de temps 
+# passe au dessus d'une tension de 0.3V ici) a stocker dans la variable 
+# largeur_flash
+
+
+
+largeur_flash = 'not this one'
+
+# Affichage
+print(largeur_flash)
