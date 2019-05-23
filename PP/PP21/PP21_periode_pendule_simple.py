43096d3ebd9668ea067e0ccd1b5474232ee681bc
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-05-23 09:59:24 +0200

    Arrivée du PP21, pour le 30/05/19

---
+++ PP/PP21/PP21_periode_pendule_simple.py
@@ -0,0 +1,12 @@
+import numpy as np
+# Lecture des donnees
+m,ell,g,x = np.loadtxt('donnees.txt',unpack=True)
+
+# A vous de trouver les deux valeurs demandees
+
+theta_max_periode_double = 'Pas'
+theta_max_x = 'Ca'
+
+
+# Affichage
+print(theta_max_periode_double,theta_max_x)
