9368715d1ec2b80e1e4bec4733d1174828c716a2
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-06-06 21:23:41 +0200

    Arrivée du PP23, pour le 13/06/19

---
+++ PP/PP23/PP23_detection_pics_FFT.py
@@ -0,0 +1,18 @@
+# Numpy pour la lecture des fichiers
+import numpy as np
+
+f,FFTe = np.loadtxt('FFTe_seule.txt',unpack=True)
+
+# Je vous conseille de faire une procedure pour trouver les positions des 
+# maxima locaux successifs.
+
+def maximas_locaux(FFT):
+    return positions_maximas_locaux
+
+# Ce qu'on doit trouver
+amplitude_3e_pic = 'No,'
+f0 = 'Not'
+Delta_f = 'this'
+
+# Et l'affichage effectif
+print(amplitude_3e_pic,f0,Delta_f)
