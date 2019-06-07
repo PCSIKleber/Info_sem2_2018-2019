f24451d4c97dfa0bab927e0c8c991ce19a7045d2
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2019-06-07 21:21:30 +0200

    Arrivée du PP24, pour le 20/06/19

---
+++ PP/PP24/PP24_bode_par_FFT.py
@@ -0,0 +1,17 @@
+# Numpy pour la lecture des fichiers
+import numpy as np
+
+f,FFTe,FFTs = np.loadtxt('FFTe_FFTs.txt',unpack=True)
+
+# Avec le precedent PythonPhysique, detectez les maximas d'amplitude du signal 
+# d'entree pour calculer le gain pour les frequences correspondant aux 
+# differents pics
+
+def GdB(FFTe,FFTs):
+    return positions_maximas_locaux
+
+liste_gains = ["Pas","ceci","Try","again"]
+
+# Et l'affichage effectif des quatre premieres valeurs (attention, j'utilise 
+# une pythonnerie...)
+print(*liste_gains[:4])
